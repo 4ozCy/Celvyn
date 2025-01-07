70ac520b289514eebc786ac5b26b3616ffff0bc8
Zcy. <157526994+4ozCy@users.noreply.github.com>
2025-01-07 14:50:25 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -134,10 +134,76 @@ loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-UNC-Test-13
 local Button = Tab:CreateButton({
   Name = "low-gfx",
   Callback = function()
-  for i,v in next, (workspace:GetDescendants()) do
- if v:IsA("Part") then v.Material = Enum.Material.SmoothPlastic
- end
+  local ToDisable = {
+	Textures = true,
+	VisualEffects = true,
+	Parts = true,
+	Particles = true,
+	Sky = true
+}
+
+local ToEnable = {
+	FullBright = false
+}
+
+local Stuff = {}
+
+for _, v in next, game:GetDescendants() do
+	if ToDisable.Parts then
+		if v:IsA("Part") or v:IsA("Union") or v:IsA("BasePart") then
+			v.Material = Enum.Material.SmoothPlastic
+			table.insert(Stuff, 1, v)
+		end
+	end
+	
+	if ToDisable.Particles then
+		if v:IsA("ParticleEmitter") or v:IsA("Smoke") or v:IsA("Explosion") or v:IsA("Sparkles") or v:IsA("Fire") then
+			v.Enabled = false
+			table.insert(Stuff, 1, v)
+		end
+	end
+	
+	if ToDisable.VisualEffects then
+		if v:IsA("BloomEffect") or v:IsA("BlurEffect") or v:IsA("DepthOfFieldEffect") or v:IsA("SunRaysEffect") then
+			v.Enabled = false
+			table.insert(Stuff, 1, v)
+		end
+	end
+	
+	if ToDisable.Textures then
+		if v:IsA("Decal") or v:IsA("Texture") then
+			v.Texture = ""
+			table.insert(Stuff, 1, v)
+		end
+	end
+	
+	if ToDisable.Sky then
+		if v:IsA("Sky") then
+			v.Parent = nil
+			table.insert(Stuff, 1, v)
+		end
+	end
 end
+
+game:GetService("TestService"):Message("Effects Disabler Script : Successfully disabled "..#Stuff.." assets / effects. Settings :")
+
+for i, v in next, ToDisable do
+	print(tostring(i)..": "..tostring(v))
+end
+
+if ToEnable.FullBright then
+    local Lighting = game:GetService("Lighting")
+    
+    Lighting.FogColor = Color3.fromRGB(255, 255, 255)
+    Lighting.FogEnd = math.huge
+    Lighting.FogStart = math.huge
+    Lighting.Ambient = Color3.fromRGB(255, 255, 255)
+    Lighting.Brightness = 5
+    Lighting.ColorShift_Bottom = Color3.fromRGB(255, 255, 255)
+    Lighting.ColorShift_Top = Color3.fromRGB(255, 255, 255)
+    Lighting.OutdoorAmbient = Color3.fromRGB(255, 255, 255)
+    Lighting.Outlines = true
+        end
    end
 })
 
@@ -305,6 +371,7 @@ local MusicIDs = getMusicIDs()
 local IsPlayingMusic = false
 local Sound
 local Volume = 1
+local Pitch = 1
 
 local function playRandomMusic()
     if not Sound or not Sound:IsDescendantOf(game.Workspace) then
@@ -362,17 +429,6 @@ local Slider = mTab:CreateSlider({
     end
 })
 
-local Button = mTab:CreateButton({
-    Name = "Skip",
-    Description = nil,
-    Callback = function()
-        if Sound then
-            Sound:Stop()
-            playRandomMusic()
-        end
-    end   
-})
-
 local Slider = mTab:CreateSlider({
     Name = "Pitch",
     Range = {0.5, 2},
@@ -384,4 +440,15 @@ local Slider = mTab:CreateSlider({
             Sound.PlaybackSpeed = Pitch
         end
     end
+})
+
+local Button = mTab:CreateButton({
+    Name = "Skip",
+    Description = nil,
+    Callback = function()
+        if Sound then
+            Sound:Stop()
+            playRandomMusic()
+        end
+    end   
 })
