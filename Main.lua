20910e7edad749c3a2747d71ad10cf1d3c9a24ff
Zcy. <157526994+4ozCy@users.noreply.github.com>
2025-01-26 16:10:05 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -353,8 +353,8 @@ local selectedOption
 
 local Dropdown = sTab:CreateDropdown({
     Name = "Server Hop Options",
-    Options = {"Normal Server", "least ping Server"},
-    CurrentOption = {"Normal Server"},
+    Options = {"None", "Random Server", "low Player Server", "low ping Server"},
+    CurrentOption = {"None"},
     MultipleOptions = false,
     SpecialType = nil,
     Callback = function(option)
@@ -365,7 +365,26 @@ local Dropdown = sTab:CreateDropdown({
 local Button = sTab:CreateButton({
     Name = "Server Hop",
     Callback = function()
-        if selectedOption == "Normal Server" then
+      if selectedOption == "None" then 
+Noti.new("error", "Celvyn Hub", "Please Choose option first", true, 5)	
+    elseif selectedOption == "Random Server" then 
+local TeleportService = game:GetService("TeleportService")
+local HttpService = game:GetService("HttpService")
+local Servers = "https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100"
+local Server, Next = nil, nil
+local function ListServers(cursor)
+    local Raw = game:HttpGet(Servers .. ((cursor and "&cursor=" .. cursor) or ""))
+    return HttpService:JSONDecode(Raw)
+end
+repeat
+    local Servers = ListServers(Next)
+    Server = Servers.data[math.random(1, (#Servers.data / 3))]
+    Next = Servers.nextPageCursor
+until Server
+if Server.playing < Server.maxPlayers and Server.id ~= game.JobId then
+    TeleportService:TeleportToPlaceInstance(game.PlaceId, Server.id, game.Players.LocalPlayer)
+  end	
+	elseif selectedOption == "low Player Server" then
             local TeleportService = game:GetService("TeleportService")
             local HttpService = game:GetService("HttpService")
             local Players = game:GetService("Players")
@@ -398,7 +417,7 @@ local Button = sTab:CreateButton({
 
             serverHop()
 
-        elseif selectedOption == "least ping Server" then
+        elseif selectedOption == "low ping Server" then
             local HTTPService = game:GetService("HttpService")
             local TeleportService = game:GetService("TeleportService")
             local StatsService = game:GetService("Stats")
@@ -433,8 +452,6 @@ local Button = sTab:CreateButton({
             end
 
             TeleportService:TeleportToPlaceInstance(placeId, lowestPingServer.id)
-        else
-            Noti.new("error", "Celvyn Hub", "Please Choose option first", true, 5)
         end
     end
 })
