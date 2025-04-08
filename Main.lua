2c261ed4ed3dac098f859426ceed1dccd1b7b91f
Zcy. <157526994+4ozCy@users.noreply.github.com>
2025-04-08 22:46:38 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -314,45 +314,36 @@ local Button = cTab:CreateButton({
 
 local sTab = Window:CreateTab("Server", "server")
 
-local selectedOption
+local Section = sTab:CreateSection("Server Section")
 
-local Dropdown = sTab:CreateDropdown({
-    Name = "Server Hop Options",
-    Options = {"None", "Random Server", "low Player Server", "low ping Server"},
-    CurrentOption = {"None"},
-    MultipleOptions = false,
-    Callback = function(option)
-        selectedOption = option
-    end
+local Button = sTab:CreateButton({
+    Name = "Random Server Hop",
+    Callback = function()
+      local HttpService = game:GetService("HttpService")
+local Servers = "https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100"
+local Server, Next = nil, nil
+local function ListServers(cursor)
+    local Raw = game:HttpGet(Servers .. ((cursor and "&cursor=" .. cursor) or ""))
+    return HttpService:JSONDecode(Raw)
+end
+repeat
+    local Servers = ListServers(Next)
+    Server = Servers.data[math.random(1, (#Servers.data / 3))]
+    Next = Servers.nextPageCursor
+until Server
+if Server.playing < Server.maxPlayers and Server.id ~= game.JobId then
+    TeleportService:TeleportToPlaceInstance(game.PlaceId, Server.id, game.Players.LocalPlayer)
+  end	
+    end 
 })
 
 local Button = sTab:CreateButton({
-    Name = "Server Hop",
+    Name = "low Player Server Hop",
     Callback = function()
-        if selectedOption == "None" then 
-            Noti.new("error", "Celvyn Hub", "Please Choose option first", true, 5)    
-        elseif selectedOption == "Random Server" then 
-            local TeleportService = game:GetService("TeleportService")
-            local HttpService = game:GetService("HttpService")
-            local Servers = "https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100"
-            local Server, Next = nil, nil
-            local function ListServers(cursor)
-                local Raw = game:HttpGet(Servers .. ((cursor and "&cursor=" .. cursor) or ""))
-                return HttpService:JSONDecode(Raw)
-            end
-            repeat
-                local Servers = ListServers(Next)
-                Server = Servers.data[math.random(1, (#Servers.data / 3))]
-                Next = Servers.nextPageCursor
-            until Server
-            if Server.playing < Server.maxPlayers and Server.id ~= game.JobId then
-                TeleportService:TeleportToPlaceInstance(game.PlaceId, Server.id, game.Players.LocalPlayer)
-            end    
-        elseif selectedOption == "low Player Server" then
-            local TeleportService = game:GetService("TeleportService")
-            local HttpService = game:GetService("HttpService")
-            local Players = game:GetService("Players")
-            local LocalPlayer = Players.LocalPlayer
+      local TeleportService = game:GetService("TeleportService")
+local HttpService = game:GetService("HttpService")
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
 
             local function getServerList()
                 local servers = {}
@@ -375,13 +366,18 @@ local Button = sTab:CreateButton({
                 if #servers > 0 then
                     TeleportService:TeleportToPlaceInstance(game.PlaceId, servers[1].id, LocalPlayer)
                 else
-                    Noti.new("info", "Celvyn Hub", "No server available.", true, 5)
+                   print("error") 
                 end
             end
 
-            serverHop()
-        elseif selectedOption == "low ping Server" then
-            local HTTPService = game:GetService("HttpService")
+         serverHop()
+    end 
+})
+
+local Button = sTab:CreateButton({
+    Name = "low Ping Server Hop",
+    Callback = function()
+      local HTTPService = game:GetService("HttpService")
             local TeleportService = game:GetService("TeleportService")
             local StatsService = game:GetService("Stats")
 
@@ -415,8 +411,8 @@ local Button = sTab:CreateButton({
             end
 
             TeleportService:TeleportToPlaceInstance(placeId, lowestPingServer.id)
-        end
-    end
+      end
+    end 
 })
 
 local Button = sTab:CreateButton({
